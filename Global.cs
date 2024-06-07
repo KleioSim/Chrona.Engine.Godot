3f8040cf47104d45a4a41509e1c95ebbc71deb05
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-06-07 16:47:18 +0800

    add CreateNativeMode

--- Global.cs
+++ Global.cs
@@ -3,30 +3,42 @@ using Chrona.Engine.Core.Events;
 using Chrona.Engine.Core.Interfaces;
 using Chrona.Engine.Core.Modders;
 using Godot;
+using System;
 using System.IO;
+using System.Linq;
 
 namespace Chrona.Engine.Godot;
 
 public partial class Global : Node
 {
-    public static string ModPath
-    {
-        get
-        {
-            var path = Path.Combine(Path.GetDirectoryName(OS.GetExecutablePath()), "mods");
-            if (OS.HasFeature("editor"))
-            {
-                path = Path.Combine(ProjectSettings.GlobalizePath("res://"), ".mods");
-            }
-
-            return path;
-        }
-    }
-
     public Chroncle Chroncle { get; private set; }
 
     public override void _Ready()
     {
-        Chroncle = new Chroncle(new Modder(ModPath));
+        var modPath = Path.Combine(ProjectSettings.GlobalizePath("user://"), "mods");
+
+        CreateNativeMode(modPath);
+        Chroncle = new Chroncle(new Modder(modPath));
+    }
+
+    private void CreateNativeMode(string modPath)
+    {
+        var sourcePath = OS.GetExecutablePath();
+        if (OS.HasFeature("editor"))
+        {
+            sourcePath = Path.Combine(ProjectSettings.GlobalizePath("res://"), ".godot/mono/temp/bin/Debug");
+        }
+
+        var files = Directory.EnumerateFiles(sourcePath, "*Native*");
+        if (files.Any())
+        {
+            var targetPath = Path.Combine(modPath, "native/dll");
+            Directory.CreateDirectory(targetPath);
+
+            foreach (var file in files)
+            {
+                File.Copy(file, Path.Combine(targetPath, Path.GetFileName(file)), true);
+            }
+        }
     }
 }
