3b0c5fc64db920322fc4df43edbdcb7efa0e7450
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-06-14 15:02:46 +0800

    Add RichText Tooltip

--- TooltipTriggers/TooltipTrigger.cs
+++ TooltipTriggers/TooltipTrigger.cs
@@ -1,6 +1,8 @@
 ﻿using Godot;
 using System;
 
+namespace Chrona.Engine.Godot.TooltipTrigger;
+
 [Tool]
 public partial class TooltipTrigger : Control
 {
@@ -9,5 +11,12 @@ public partial class TooltipTrigger : Control
     public override string _GetTooltip(Vector2 atPosition)
     {
         return funcGetToolTipString != null ? funcGetToolTipString() : this.TooltipText;
+    }
+
+    public override Control _MakeCustomTooltip(string forText)
+    {
+        Control tooltip = ResourceLoader.Load<PackedScene>("res://addons/Chrona.Engine.Godot/TooltipTriggers/TooltipPanel.tscn").Instantiate() as Control;
+        tooltip.GetNode<RichTextLabel>("RichTextLabel").Text = forText;
+        return tooltip;
     }
 }
