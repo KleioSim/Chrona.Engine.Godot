3d49018511cf1ef793b70b594dac2df96fba66f6
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-10-14 21:39:28 +0800

    fixed isuess

--- IView.cs
+++ IView.cs
@@ -4,21 +4,24 @@ using System.Collections.Generic;
 
 namespace Chrona.Engine.Godot;
 
-public interface IView
+public interface IView<T> : IView
 {
-    static Dictionary<IView, uint> view2Label = new Dictionary<IView, uint>();
-
     bool IsDirty()
     {
         if (!view2Label.TryGetValue(this, out uint value))
         {
-            view2Label.Add(this, Decorator<IData>.Label);
+            view2Label.Add(this, Decorator<T>.Label);
             var node = this as Node;
             node.Connect(Node.SignalName.TreeExiting, Callable.From(() => view2Label.Remove(this)));
             return true;
         }
 
-        view2Label[this] = Decorator<IData>.Label;
-        return value != Decorator<IData>.Label;
+        view2Label[this] = Decorator<T>.Label;
+        return value != Decorator<T>.Label;
     }
+}
+
+public interface IView
+{
+    static Dictionary<IView, uint> view2Label = new Dictionary<IView, uint>();
 }
