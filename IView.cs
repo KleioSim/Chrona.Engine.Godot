230e4107629202a87af4988ca8cb690be61cbd41
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-10-15 18:08:39 +0800

    Add IsSelfDirty

--- IView.cs
+++ IView.cs
@@ -6,8 +6,33 @@ namespace Chrona.Engine.Godot;
 
 public interface IView<T> : IView
 {
+    bool IsSelfDirty
+    {
+        get
+        {
+            return selfDirtyViews.Contains(this);
+        }
+        set
+        {
+            if (value)
+            {
+                selfDirtyViews.Add(this);
+            }
+            else
+            {
+                selfDirtyViews.Remove(this);
+            }
+        }
+    }
+
     bool IsDirty()
     {
+        if (IsSelfDirty)
+        {
+            selfDirtyViews.Remove(this);
+            return true;
+        }
+
         if (!view2Label.TryGetValue(this, out uint value))
         {
             view2Label.Add(this, Decorator<T>.Label);
@@ -19,9 +44,11 @@ public interface IView<T> : IView
         view2Label[this] = Decorator<T>.Label;
         return value != Decorator<T>.Label;
     }
+
 }
 
 public interface IView
 {
     static Dictionary<IView, uint> view2Label = new Dictionary<IView, uint>();
+    static HashSet<IView> selfDirtyViews = new HashSet<IView>();
 }
