9f20d45ea5e88774b3e77b7d4d12df3f6b7a58db
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-11-15 18:08:50 +0800

    Add Container

--- ItemContainer.cs
+++ ItemContainer.cs
@@ -5,71 +5,71 @@ using System.Linq;
 
 namespace Chrona.Engine.Godot;
 
-public interface IItemView
+public interface IItem
 {
     object Id { get; set; }
 }
 
-public class ItemContainer<T> where T : Control, IItemView
-{
-    private Func<InstancePlaceholder> itemPlaceHolder;
+//public class ItemContainer<T> where T : Control, IItemView
+//{
+//    private Func<InstancePlaceholder> itemPlaceHolder;
 
-    public Action<T> OnAddedItem { get; set; }
+//    public Action<T> OnAddedItem { get; set; }
 
-    public ItemContainer(Func<InstancePlaceholder> placeholder)
-    {
-        this.itemPlaceHolder = placeholder;
-    }
+//    public ItemContainer(Func<InstancePlaceholder> placeholder)
+//    {
+//        this.itemPlaceHolder = placeholder;
+//    }
 
-    public IEnumerable<T> GetCurrentItems()
-    {
-        return itemPlaceHolder().GetParent().GetChildren().Select(x => x.GetNodeOrNull<T>("."))
-            .Where(x => x != null);
-    }
+//    public IEnumerable<T> GetCurrentItems()
+//    {
+//        return itemPlaceHolder().GetParent().GetChildren().Select(x => x.GetNodeOrNull<T>("."))
+//            .Where(x => x != null);
+//    }
 
-    public T AddItem(object taskId)
-    {
-        var item = itemPlaceHolder().CreateInstance() as T;
-        item.Id = taskId;
-        OnAddedItem?.Invoke(item);
+//    public T AddItem(object taskId)
+//    {
+//        var item = itemPlaceHolder().CreateInstance() as T;
+//        item.Id = taskId;
+//        OnAddedItem?.Invoke(item);
 
-        return item;
-    }
+//        return item;
+//    }
 
-    public void RemoveItem(T item)
-    {
-        item.QueueFree();
-    }
+//    public void RemoveItem(T item)
+//    {
+//        item.QueueFree();
+//    }
 
-    public IEnumerable<T> Refresh(HashSet<object> keys)
-    {
-        var taskViewDict = GetCurrentItems().ToDictionary(x => x.Id, x => x);
+//    public IEnumerable<T> Refresh(HashSet<object> keys)
+//    {
+//        var taskViewDict = GetCurrentItems().ToDictionary(x => x.Id, x => x);
 
-        var needRemoves = new Queue<object>(taskViewDict.Keys.Except(keys));
-        var needAdds = new Queue<object>(keys.Except(taskViewDict.Keys));
+//        var needRemoves = new Queue<object>(taskViewDict.Keys.Except(keys));
+//        var needAdds = new Queue<object>(keys.Except(taskViewDict.Keys));
 
-        var addedItems = new List<T>();
+//        var addedItems = new List<T>();
 
-        while (needAdds.TryDequeue(out object key))
-        {
-            if (needRemoves.TryDequeue(out object replaceKey))
-            {
-                var Item = taskViewDict[replaceKey];
-                Item.Id = key;
-            }
-            else
-            {
-                var newItem = AddItem(key);
-                addedItems.Add(newItem);
-            }
-        }
+//        while (needAdds.TryDequeue(out object key))
+//        {
+//            if (needRemoves.TryDequeue(out object replaceKey))
+//            {
+//                var Item = taskViewDict[replaceKey];
+//                Item.Id = key;
+//            }
+//            else
+//            {
+//                var newItem = AddItem(key);
+//                addedItems.Add(newItem);
+//            }
+//        }
 
-        while (needRemoves.TryDequeue(out object key))
-        {
-            var item = taskViewDict[key];
-            RemoveItem(item);
-        }
+//        while (needRemoves.TryDequeue(out object key))
+//        {
+//            var item = taskViewDict[key];
+//            RemoveItem(item);
+//        }
 
-        return addedItems;
-    }
-}
+//        return addedItems;
+//    }
+//}
